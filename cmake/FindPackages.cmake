f74decbda0655226a756aa024aa53941a9230d35
lyt-s <1814666859@qq.com>
2023-09-28 17:30:55 +0800

    修改：     CMakeLists.txt 	新文件：   cmake/FindPackages.cmake 	修改：     tests/test_config.cc

---
+++ cmake/FindPackages.cmake
@@ -0,0 +1,15 @@
+set(THIRD_PARTY_DIR ${CMAKE_CURRENT_SOURCE_DIR}/third_party)
+
+# boost
+list(APPEND CMAKE_PREFIX_PATH ${THIRD_PARTY_DIR}/boost)
+find_package(Boost REQUIRED) # COMPONENTS ALL
+message(STATUS "Find Boost in ${Boost_DIR}")
+message(STATUS "Boost version: ${Boost_VERSION}")
+
+# yaml-cpp
+list(APPEND CMAKE_PREFIX_PATH ${THIRD_PARTY_DIR}/yaml-cpp)
+find_package(yaml-cpp REQUIRED) # COMPONENTS ALL
+message(STATUS "Find yaml-cpp in ${yaml-cpp_DIR}")
+message(STATUS "yaml-cpp version: ${yaml-cpp_VERSION}")
+set(yaml-cpp_LIBRARIES ${THIRD_PARTY_DIR}/yaml-cpp/lib/libyaml-cpp.a)
+set(yaml-cpp_INCLUDE_DIRS ${THIRD_PARTY_DIR}/yaml-cpp/include)
--- tests/test_config.cc
+++ tests/test_config.cc
@@ -1,15 +1,40 @@
 #include <yaml-cpp/yaml.h>
-#include "../sylar/config.h"
-#include "../sylar/log.h"
+#include <cstddef>
+#include <string>
+#include "sylar/config.h"
+#include "sylar/log.h"
 #include "yaml-cpp/node/node.h"
 
 sylar::ConfigVar<int>::ptr g_int_value_config =
     sylar::Config::Lookup("system.port", (int)8080, "system port");
 
-void print_yaml() { YAML::Node root; }
+void print_yaml(const YAML::Node &node, int level) {
+  if (node.IsScalar()) {
+    SYLAR_LOG_INFO(SYLAR_LOG_ROOT())
+        << std::string(level * 4, ' ') << node.Scalar() << " - " << node.Type() << " - " << level;
+  } else if (node.IsNull()) {
+    SYLAR_LOG_INFO(SYLAR_LOG_ROOT())
+        << std::string(level * 4, ' ') << node.Scalar() << " - " << node.Type() << " - " << level;
+  } else if (node.IsMap()) {
+    for (auto it = node.begin(); it != node.end(); ++it) {
+      SYLAR_LOG_INFO(SYLAR_LOG_ROOT())
+          << std::string(level * 4, ' ') << it->first << ' ' << it->second.Type() << ' ' << level;
+      print_yaml(it->second, level + 1);
+    }
+  } else if (node.IsSequence()) {
+    for (size_t i = 0; i < node.size(); ++i) {
+      SYLAR_LOG_INFO(SYLAR_LOG_ROOT())
+          << std::string(level * 4, ' ') << i << " - " << node[i].Type() << " - " << level;
+      print_yaml(node[i], level + 1);
+    }
+  }
+}
 void test_yaml() {
-  YAML::Node root = YAML::LoadFile("../template/bin/conf/log.yaml");
-  SYLAR_LOG_INFO(SYLAR_LOG_ROOT()) << root;
+  YAML::Node root = YAML::LoadFile("../template/bin/conf/log.yml");
+  SYLAR_LOG_INFO(SYLAR_LOG_ROOT()) << root["test"].IsDefined();
+  SYLAR_LOG_INFO(SYLAR_LOG_ROOT()) << root["logs"].IsDefined();
+  // SYLAR_LOG_INFO(SYLAR_LOG_ROOT()) << root;
+  print_yaml(root, 0);
 }
 int main(int argc, char **argv) {
   SYLAR_LOG_INFO(SYLAR_LOG_ROOT()) << g_int_value_config->getValue();
