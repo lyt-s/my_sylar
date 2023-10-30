0a61035e42e4b67092436dd533f493cf60283214
lyt-s <1814666859@qq.com>
2023-10-30 17:53:23 +0800

    添加注释， 	修改：     sylar/hook.cc 	修改：     sylar/macro.h  将LICK  修改为LIKE 	修改：     sylar/socket.cc 	修改：     sylar/timer.cc 	修改：     sylar/timer.h

--- sylar/timer.cc
+++ sylar/timer.cc
@@ -7,7 +7,8 @@
 #include "util.h"
 
 namespace sylar {
-bool Timer::Comparator::operator()(const Timer::ptr &lhs, const Timer::ptr &rhs) const {
+bool Timer::Comparator::operator()(const Timer::ptr &lhs,
+                                   const Timer::ptr &rhs) const {
   if (!lhs && !rhs) {
     return false;
   }
@@ -26,7 +27,8 @@ bool Timer::Comparator::operator()(const Timer::ptr &lhs, const Timer::ptr &rhs)
   return lhs.get() < rhs.get();
 }
 
-Timer::Timer(uint64_t ms, std::function<void()> cb, bool resurring, TimerManager *manager)
+Timer::Timer(uint64_t ms, std::function<void()> cb, bool resurring,
+             TimerManager *manager)
     : m_recurring(resurring), m_ms(ms), m_cb(cb), m_manager(manager) {
   m_next = sylar::GetCurrentMS() + m_ms;
 }
@@ -56,14 +58,18 @@ bool Timer::refresh() {
     return false;
   }
 
-  //  先移除掉，重置时间，然后塞回去--？？why
-  // operator 是基于时间来做的，如果直接改时间，会影响他的比较位置，整个数据结构都会有问题--todo
+  //  先移除掉，重置时间，然后塞回去-- ？？why --
+  //  set不提供直接存取元素的任何操作函数
+  // operator
+  // 是基于时间来做的，如果直接改时间，会影响他的比较位置，整个数据结构都会有问题,排序准则
   m_manager->m_timers.erase(it);
   m_next = sylar::GetCurrentMS() + m_ms;
   m_manager->m_timers.insert(shared_from_this());
   return true;
 }
+
 bool Timer::reset(uint64_t ms, bool from_now) {
+  /// 不从现在开始计时
   if (ms == m_ms && !from_now) {
     return true;
   }
@@ -77,14 +83,14 @@ bool Timer::reset(uint64_t ms, bool from_now) {
     return false;
   }
 
-  // 先移除掉，重置时间，然后塞回去--？？why
-  // operator 是基于时间来做的，如果直接改时间，会影响他的比较位置，整个数据结构都会有问题--todo
   m_manager->m_timers.erase(it);
-
   uint64_t start = 0;
+  // 是否从当前时间开始计算
   if (from_now) {
+    /// 改为现在开始
     start = sylar::GetCurrentMS();
   } else {
+    /// 获取起始时间
     start = m_next - m_ms;
   }
   m_ms = ms;
@@ -97,10 +103,11 @@ bool Timer::reset(uint64_t ms, bool from_now) {
 TimerManager::TimerManager() { m_previousTime = sylar::GetCurrentMS(); }
 TimerManager::~TimerManager() {}
 
-Timer::ptr TimerManager::addTimer(uint64_t ms, std::function<void()> cb, bool recurring) {
+Timer::ptr TimerManager::addTimer(uint64_t ms, std::function<void()> cb,
+                                  bool recurring) {
   Timer::ptr timer(new Timer(ms, cb, recurring, this));
-  // Timer::ptr timer = std::make_shared<Timer>(ms, cb, recurring, this);  // timer
-  // 私有构造，无法使用make_shared
+  // Timer::ptr timer = std::make_shared<Timer>(ms, cb, recurring, this);  //
+  // timer 私有构造，无法使用make_shared
 
   RWMutexType::WriteLock lock(m_mutex);
   addTimer(timer, lock);
@@ -114,24 +121,29 @@ static void OnTimer(std::weak_ptr<void> weak_cond, std::function<void()> cb) {
   }
 }
 
-Timer::ptr TimerManager::addConditionTimer(uint64_t ms, std::function<void()> cb,
-                                           std::weak_ptr<void> weak_cond, bool recurring) {
+Timer::ptr TimerManager::addConditionTimer(uint64_t ms,
+                                           std::function<void()> cb,
+                                           std::weak_ptr<void> weak_cond,
+                                           bool recurring) {
   return addTimer(ms, std::bind(&OnTimer, weak_cond, cb), recurring);
 }
 
 uint64_t TimerManager::getNextTimer() {
   RWMutexType::ReadLock lock(m_mutex);
   m_tickled = false;
-  // todo
+
   if (m_timers.empty()) {
+    // Value = 18446744073709551615 (0xffffffffffffffff)
     return ~0ull;
   }
 
   const Timer::ptr &next = *m_timers.begin();
   uint64_t now_ms = sylar::GetCurrentMS();
+  /// 已经超时，需要执行了，，直接返回 0
   if (now_ms >= next->m_next) {
     return 0;
   } else {
+    /// 返回到m_next时间，需要等待的时间
     return next->m_next - now_ms;
   }
 }
@@ -146,21 +158,25 @@ void TimerManager::listExpiredCb(std::vector<std::function<void()>> &cbs) {
     }
   }
   RWMutexType::WriteLock lock(m_mutex);
-
+  /// 检查是否时间调后了，会导致任务无法按时执行。
   bool rollover = detextClockRollover(now_ms);
+  /// 没有重调时间，且超时任务还没到达
   if (!rollover && ((*m_timers.begin())->m_next > now_ms)) {
     return;
   }
+  /// 参考时间
   Timer::ptr now_timer(new Timer(now_ms));
-  // Timer::ptr now_timer = std::make_shared<Timer>(now_ms);
-  // todo
+  // lower_bound(
+  // begin,end,num)：从数组的begin位置到end-1位置二分查找第一个大于或等于num的数字，找到返回该数字的地址，不存在则返回end。
+  // 找到所有需要执行的定时器
   auto it = rollover ? m_timers.end() : m_timers.lower_bound(now_timer);
-
+  // 处理 (*it)->m_next == now 的情况，
   while (it != m_timers.end() && (*it)->m_next == now_ms) {
     ++it;
   }
   expired.insert(expired.begin(), m_timers.begin(), it);
   m_timers.erase(m_timers.begin(), it);
+  /// 预分配内存
   cbs.reserve(expired.size());
 
   for (auto &timer : expired) {
@@ -168,13 +184,16 @@ void TimerManager::listExpiredCb(std::vector<std::function<void()>> &cbs) {
     if (timer->m_recurring) {
       timer->m_next = now_ms + timer->m_ms;
       m_timers.insert(timer);
-
     } else {
+      /// 应该就析构了，不需要清空。
       timer->m_cb = nullptr;
     }
   }
 }
 
+// 当TimerManager检测到新添加的定时器的超时时间比当前最小的定时器还要小时，TimerManager通过这个方法来通知IOManager立刻更新当前的epoll_wait超时，
+// 否则新添加的定时器的执行时间将不准确。实际实现时，只需要在onTimerInsertedAtFront()
+// 方法内执行一次tickle就行了，tickle之后，epoll_wait会立即退出，并重新从TimerManager中获取最近的超时时间，这时拿到的超时时间就是新添加的最小定时器的超时时间了。
 void TimerManager::addTimer(Timer::ptr timer, RWMutexType::WriteLock &lock) {
   auto it = m_timers.insert(timer).first;
   bool at_front = (it == m_timers.begin()) && !m_tickled;
@@ -183,6 +202,7 @@ void TimerManager::addTimer(Timer::ptr timer, RWMutexType::WriteLock &lock) {
   }
   lock.unlock();
   // 不需要来回修改，只修改一次
+  /// 是否在最靠前
   if (at_front) {
     onTimerInsertedAtFront();
   }
@@ -191,9 +211,11 @@ void TimerManager::addTimer(Timer::ptr timer, RWMutexType::WriteLock &lock) {
 // 传进来的时间比他小，并且比他一个小时之前按还小，有问题
 bool TimerManager::detextClockRollover(uint64_t now_ms) {
   bool rollover = false;
+  // 现在的时间比上次执行的时间还小，并且比上次执行的时间的一小时之前还小，有问题，会出现吗？？？
   if (now_ms < m_previousTime && now_ms < (m_previousTime - 60 * 60 * 1000)) {
     rollover = true;
   }
+  // 重置 上次执行的时间
   m_previousTime = now_ms;
   return rollover;
 }
--- sylar/timer.h
+++ sylar/timer.h
@@ -10,24 +10,56 @@
 namespace sylar {
 class TimerManager;
 
+/**
+ * @brief 定时器
+ */
 class Timer : public std::enable_shared_from_this<Timer> {
   friend class TimerManager;
 
  public:
   typedef std::shared_ptr<Timer> ptr;
-
+  /**
+   * @brief 取消定时器
+   *  从TimerManager的m_timers中找到对应的Timer::ptr ,erase
+   * @return true
+   * @return false
+   */
   bool cancel();
+
+  /**
+   * @brief 刷新设置定时器的执行时间
+   *
+   * @return true
+   * @return false
+   */
   bool refresh();
+
+  /**
+   * @brief 重置定时器时间
+   *
+   * @param ms 定时器执行间隔时间(毫秒)
+   * @param from_now  是否从当前时间开始计算
+   * @return true
+   * @return false
+   */
   bool reset(uint64_t ms, bool from_now);
 
  private:
-  Timer(uint64_t ms, std::function<void()> cb, bool resurring, TimerManager *manager);
+  /**
+   * @brief 构造函数
+   * @param[in] ms 定时器执行间隔时间
+   * @param[in] cb 回调函数
+   * @param[in] recurring 是否循环
+   * @param[in] manager 定时器管理器
+   */
+  Timer(uint64_t ms, std::function<void()> cb, bool resurring,
+        TimerManager *manager);
   Timer(uint64_t next);
 
  private:
   bool m_recurring = false;  // 是否循环定时器
-  uint64_t m_ms = 0;         //执行周期
-  uint64_t m_next = 0;       //精确的执行时间
+  uint64_t m_ms = 0;         // 执行周期
+  uint64_t m_next = 0;       // 精确的执行时间
   std::function<void()> m_cb;
   TimerManager *m_manager = nullptr;
 
@@ -46,25 +78,66 @@ class TimerManager {
   TimerManager();
   virtual ~TimerManager();
 
-  Timer::ptr addTimer(uint64_t ms, std::function<void()> cb, bool recurring = false);
-  Timer::ptr addConditionTimer(uint64_t ms, std::function<void()> cb, std::weak_ptr<void> weak_cond,
+  /**
+   * @brief 添加定时器
+   * @param[in] ms 定时器执行间隔时间
+   * @param[in] cb 定时器回调函数
+   * @param[in] recurring 是否循环定时器
+   */
+  Timer::ptr addTimer(uint64_t ms, std::function<void()> cb,
+                      bool recurring = false);
+  /**
+   * @brief 添加条件定时器
+   * @param[in] ms 定时器执行间隔时间
+   * @param[in] cb 定时器回调函数
+   * @param[in] weak_cond 条件
+   * @param[in] recurring 是否循环
+   */
+  Timer::ptr addConditionTimer(uint64_t ms, std::function<void()> cb,
+                               std::weak_ptr<void> weak_cond,
                                bool recurring = false);
+  /**
+   * @brief 到最近一个定时器执行的时间间隔(毫秒)
+   *
+   * @return uint64_t
+   */
   uint64_t getNextTimer();
+
+  /**
+   * @brief 获取需要执行的定时器的回调函数列表
+   *
+   * @param[out] cbs 回调函数数组
+   */
   void listExpiredCb(std::vector<std::function<void()>> &cbs);
+  /**
+   * @brief 是否有定时器
+   */
   bool hasTimer();
 
  protected:
+  /**
+   * @brief 当有新的定时器插入到定时器的首部,执行该函数,IoManager来实现
+   */
   virtual void onTimerInsertedAtFront() = 0;
+  /**
+   * @brief 将定时器添加到管理器中
+   */
   void addTimer(Timer::ptr val, RWMutexType::WriteLock &lock);
 
  private:
+  /**
+   * @brief 检测服务器时间是否被调后了
+   */
   bool detextClockRollover(uint64_t now_ms);
 
  private:
   RWMutexType m_mutex;
-  // 这个参数不代表映射对象，而是传递一个比较结构体 //todo
+  /// 这个参数不代表映射对象，而是传递一个比较结构体
+  /// 定时器集合
   std::set<Timer::ptr, Timer::Comparator> m_timers;
+  /// 是否触发onTimerInsertedAtFront
   bool m_tickled = false;
+  /// 上次执行时间
   uint64_t m_previousTime = 0;
 };
 
